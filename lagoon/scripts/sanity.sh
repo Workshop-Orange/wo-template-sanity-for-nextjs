b141d2a48f30ddfe0b1ae760a3c7853c5e212546
Bryan Gruneberg <hello@bryangruneberg.com>
2024-01-12 09:33:36 -0600

    Move sanity config file

--- lagoon/scripts/sanity.sh
+++ lagoon/scripts/sanity.sh
@@ -2,10 +2,22 @@
 
 echo Your studio will be available shortly: $LAGOON_ROUTE
 
+if [ -d "/home/.config" ]; then
+	fix-permissions /home/.config
+fi
+
+if [ -d "/app/.cache" ]; then
+	fix-permissions /app/.cache
+fi
+
+if [ -d "/app/public" ]; then
+	fix-permissions /app/public
+fi
+
 if [ ! -z "$LAGOON_ENVIRONMENT_TYPE" ]; then
-	echo "$LAGOON_ENVIRONMENT_TYPE: NextJS is starting"
+	echo "$LAGOON_ENVIRONMENT_TYPE: Sanity is starting"
 	npm run start | tee /tmp/sanity.log
 else
-	echo "Local: NextJS is starting"
+	echo "Local: Sanity is starting"
 	npm run dev | tee /tmp/sanity.log
 fi

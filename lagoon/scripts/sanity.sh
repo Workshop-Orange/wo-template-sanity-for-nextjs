2ace8bb9388711f5a51d26fb76aa58f38dea13fe
Bryan Gruneberg <hello@bryangruneberg.com>
2024-01-11 22:19:43 -0600

    Initial commit

--- lagoon/scripts/sanity.sh
+++ lagoon/scripts/sanity.sh
@@ -5,7 +5,7 @@ echo Your studio will be available shortly: $LAGOON_ROUTE
 echo "$LAGOON_ENVIRONMENT_TYPE: NextJS is starting"
 
 if [ "$LAGOON_ENVIRONMENT_TYPE" == "production" ]; then
-	npm run start
+	npm run start | tee /tmp/sanity.log
 else
-	npm run dev
+	npm run dev | tee /tmp/sanity.log
 fi
